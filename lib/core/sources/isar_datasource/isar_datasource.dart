91451c905fa9b199d2d8845a9f5b5d085a697569
TesteurManiak <rouxguillaume8@gmail.com>
2023-07-28 16:16:06 +0200

    feat: basic instance getter

--- lib/core/sources/isar_datasource/isar_datasource.dart
+++ lib/core/sources/isar_datasource/isar_datasource.dart
@@ -1,7 +1,22 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_manga_reader/core/sources/local_datasource/local_datasource.dart';
+import 'package:isar/isar.dart';
 import 'package:manga_reader_core/manga_reader_core.dart';
+import 'package:path_provider/path_provider.dart';
 
 class IsarDatasource implements LocalDatasource {
+  static const _instanceName = 'isar_datasource';
+
+  @protected
+  Future<Isar> getInstance() async {
+    return Isar.getInstance(_instanceName) ??
+        await Isar.open(
+          [],
+          directory: (await getApplicationDocumentsDirectory()).path,
+          name: _instanceName,
+        );
+  }
+
   @override
   Future<Manga?> getManga(String mangaId) {
     // TODO(Guillaume): implement getManga
