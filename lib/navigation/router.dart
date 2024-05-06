721bef158c936c00567d2d6fd1906496a1007303
Guillaume Roux <rouxguillaume8@gmail.com>
2024-05-06 09:47:03 +0200

    feat: added navigatorKey

--- lib/navigation/router.dart
+++ lib/navigation/router.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_manga_reader/features/chapter_viewer/navigation/route.dart'
     as viewer;
 import 'package:flutter_manga_reader/features/details/navigation/route.dart'
@@ -7,8 +8,11 @@ import 'package:flutter_manga_reader/features/home/navigation/route.dart'
     as home;
 import 'package:go_router/go_router.dart';
 
+final navigatorKey = GlobalKey<NavigatorState>();
+
 GoRouter createRouter({bool debugLogDiagnostics = kDebugMode}) {
   return GoRouter(
+    navigatorKey: navigatorKey,
     debugLogDiagnostics: debugLogDiagnostics,
     routes: [
       ...home.$appRoutes,
